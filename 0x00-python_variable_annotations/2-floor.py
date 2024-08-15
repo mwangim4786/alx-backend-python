0716215ac223f522b24a025b19c848ee7343b63d
Martin Kihungi <mwangim4786@gmail.com>
2024-08-16 02:20:48 +0300

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,7 +5,4 @@
 
 def floor(n: float) -> int:
 	''' Return largest int value less than or equal to n. '''
-	if n >= 0:
-		return(int(n))
-	else:
-		return(int(n) - 1)
+	return int(n) if n >= 0 else int(n) - 1
